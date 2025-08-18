4b23d5421bec4b549b9f32c86de5db3e0c9299b0
Danny00117 <charly.code0012@gmail.com>
2025-08-18 13:00:25 -0600

    Update _and.v

--- Practicas/Compuerta/_and.v
+++ Practicas/Compuerta/_and.v
@@ -1,6 +1,6 @@
 //Lozano Carlos Daniel 224003566
+//https://github.com/CharlyCode0012/ArquitecturaDeComputadoras.git
 //1.- definir modulo y uss i/O
-
 module _and (input A, input B, output C);
 //2.- declarar señales/elementos internos
 
