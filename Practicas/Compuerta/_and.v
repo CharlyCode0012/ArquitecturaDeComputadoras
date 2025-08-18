0ee5ddc44c2948cace502b08e8836b028e3bde24
Danny00117 <charly.code0012@gmail.com>
2025-08-18 12:42:28 -0600

    Compuerta AND

---
+++ Practicas/Compuerta/_and.v
@@ -0,0 +1,14 @@
+//Lozano Carlos Daniel 224003566
+//1.- definir modulo y uss i/O
+
+module _and (input A, input B, output C);
+//2.- declarar señales/elementos internos
+
+/*3.-comportamiento/implementacion del modulo 
+(asiganciones, instancias, conexiones)
+*/
+assign C = A & B;
+//4.- Compilar y si ya esta la palomita verde vamos a library
+//5.- 
+
+endmodule
